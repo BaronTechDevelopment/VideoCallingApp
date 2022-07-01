d750e90e6efde85c20ea47d142c0f6e8b49dc69b
saifurrehmanahmed <100399633+BaronTechDevelopment@users.noreply.github.com>
2022-07-01 18:00:28 +0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+const App = () =>  {
+  
+  return (
+    <SafeAreaView style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+      <Text>Video Calling App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
